bbdda421ec97eaf87915f283697339cf5bd167f4
Mr Langa <user@Marks-MacBook-Air.local>
2020-10-05 19:31:12 +0200

    Update animations

--- public/js/animate.js
+++ public/js/animate.js
@@ -9,10 +9,10 @@ init = () => {
 }
 
 moveRight = () => {
-    pos == 2000 ? clearInterval(setInterval(moveRight, 5)) :
+    pos == 100 ? clearInterval(setInterval(moveRight, 5)) :
         imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
         animate = setTimeout(moveRight, 5);
-        pos += 10;
+        //pos += 10;
         redirect("./portfolio.html");
 }
 
@@ -22,3 +22,88 @@ stop = () => {
 }
 
 window.onload = init;
+
+// animate progress
+
+loadBar = (id, skillPercent, speed) => {
+    const elem = document.getElementById(id)
+    let width = 20;
+    var id = setInterval(fill, speed);
+
+    function fill(){
+        width >= skillPercent ? clearInterval(id): width++; elem.style.width = width + "%";
+        }
+    }
+
+const skillSet = [
+    {
+        id: "#html5-progressBar",
+        percentage: 98,
+        loadTime: 30
+    },
+    {
+        id: "#css3-progressBar",
+        percentage: 85,
+        loadTime: 30
+    },
+    {
+        id: "#js-progressBar",
+        percentage: 60,
+        loadTime: 30
+    },
+    {
+        id: "#php-progressBar",
+        percentage: 50,
+        loadTime: 30
+    },
+    {
+        id: "#microsoft-progressBar",
+        percentage: 60,
+        loadTime: 30
+    },
+    {
+        id: "#java-progressBar",
+        percentage: 80,
+        loadTime: 30
+    },
+    {
+        id: "#cpp-progressBar",
+        percentage: 83,
+        loadTime: 30
+    },
+    {
+        id: "#python-progressBar",
+        percentage: 70,
+        loadTime: 30
+    },
+    {
+        id: "#ae-progressBar",
+        percentage: 60,
+        loadTime: 30
+    },
+    {
+        id: "#ps-progressBar",
+        percentage: 55,
+        loadTime: 30
+    },
+    {
+        id: "#ai-progressBar",
+        percentage: 80,
+        loadTime: 30
+    },
+    {
+        id: "#leadership-progressBar",
+        percentage: 95,
+        loadTime: 30
+    },
+    {
+        id: "#speaking-progressBar",
+        percentage: 90,
+        loadTime: 30
+    }
+];
+
+skillSet.forEach(function(skill) {
+    loadBar(skill.id, skill.percentage, skill.loadTime);
+});
+
