961466d0c7bd5c072ba9fb5012369db5d2c6dabf
Mr Langa <user@Marks-MacBook-Air.local>
2020-10-08 20:53:16 +0200

    update

--- public/js/test.js
+++ public/js/test.js
@@ -1,15 +1,25 @@
-function successGeo(position){
-    alert('lat: ' + position.coords.latitude);
-    alert('long: ' + position.coords.longitude);
-    alert('accuracy: ' + position.coords.accuracy);
-    //alert('We found the treasure: ' + position)
-}
-function failGeo(error){
-    alert('Houston, we have a problem: ' + position);
-}
+// function successGeo(position){
+//     alert('lat: ' + position.coords.latitude);
+//     alert('long: ' + position.coords.longitude);
+//     alert('accuracy: ' + position.coords.accuracy);
+//     //alert('We found the treasure: ' + position)
+// }
+// function failGeo(error){
+//     alert('Houston, we have a problem: ' + position);
+// }
 
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(successGeo, failGeo);
-}else{
-    alert('browser does not support geolocation :(');
-}
+// if (navigator.geolocation){
+//     navigator.geolocation.getCurrentPosition(successGeo, failGeo);
+// } else {
+//     alert('browser does not support geolocation :(');
+// }
+
+
+fetch('js/skills.json').then(function(response){
+    return response.json();
+}).then(function (obj){
+    console.log(obj)
+}).catch(function(error){
+    console.log('Something went wrong.');
+    console.log(error);
+});
